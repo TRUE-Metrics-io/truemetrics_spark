592722dd1a4b72a42704148f30aed64dd714ed85
Benjamin Feifke <benjamin@truemetrics.io>
2023-12-14 17:11:01 +0100

    Extracted some functions to utils file.

--- src/filter_merge_glue_job.py
+++ src/filter_merge_glue_job.py
@@ -1,4 +1,3 @@
-import math
 from typing import Union
 from pyspark import RDD
 
@@ -7,191 +6,8 @@ from pyspark.sql.functions import explode, arrays_zip, col, expr, first, from_un
 from base_glue_job import BaseGlueJob
 
 from awsglue.dynamicframe import DynamicFrame
+import filter_merge_glue_job_utils
 
-
-target_sensor_schema = {
-    "acc_x_sen": "double",
-    "acc_y_sen": "double",
-    "acc_z_sen": "double",
-    "angvel_x_sen": "double",
-    "angvel_y_sen": "double",
-    "angvel_z_sen": "double",
-    "mag_x_sen": "double",
-    "mag_y_sen": "double",
-    "mag_z_sen": "double",
-    "log": "string",
-    "confidence": "int",
-    "pres_abs": "double",
-    "ang_lat": "double",
-    "accu_ang_bearing": "double",
-    "vel": "double",
-    "ang_bearing": "double",
-    "accu_ver": "double",
-    "accu_hor": "double",
-    "accu_vel": "double",
-    "alt": "double",
-    "ang_lon": "double",
-    "ang_lat_gnss": "double",
-    "accu_ang_bearing_gnss": "double",
-    "vel_gnss": "double",
-    "ang_bearing_gnss": "double",
-    "accu_ver_gnss": "double",
-    "accu_hor_gnss": "double",
-    "accu_vel_gnss": "double",
-    "alt_gnss": "double",
-    "ang_lon_gnss": "double",
-    "customermetadata": "string",
-    "status": "string",
-    "svid": "int",
-    "type_constellation": "int",
-    "time_offset_ns": "double",
-    "state": "int",
-    "t_received_sv_ns": "double",
-    "t_received_sv_unc_ns": "double",
-    "cn0_db": "double",
-    "baseband_cn0_db": "double",
-    "pseudorange": "double",
-    "unc_pseudorange": "double",
-    "state_d_range_acc": "int",
-    "d_range_acc": "double",
-    "d_range_unc_acc": "double",
-    "f_carrier": "double",
-    "cycles_carrier": "double",
-    "phase_carrier": "double",
-    "unc_phase_carrier": "double",
-    "indicator_multipath": "int",
-    "snr_in_db": "double",
-    "auto_gain_ctrl_level": "double",
-    "type_code": "string",
-    "bias_full_inter_signal_ns": "double",
-    "unc_bias_full_inter_signal_ns": "double",
-    "bias_sat_inter_signal_ns": "double",
-    "unc_bias_sat_inter_signal_ns": "double",
-    "bias_code_phase_ns": "double",
-    "unc_bias_code_phase_ns": "double",
-    "bias_carrier_phase_ns": "double",
-    "unc_bias_carrier_phase_ns": "double",
-    "multipath_indic": "int",
-    "snr_in_db_gnss": "double",
-    "auto_gain_ctrl_level_gnss": "double",
-    "type_code_gnss": "int",
-    "bias_full_inter_signal_ns_gnss": "double",
-    "unc_bias_full_inter_signal_ns_gnss": "double",
-    "bias_sat_inter_signal_ns_gnss": "double",
-    "unc_bias_sat_inter_signal_ns_gnss": "double",
-    "level_battery": "double",
-    "status_battery": "string",
-    "health_battery": "string",
-    "count_step": "double",
-    "status_connectivity": "string",
-    "activity": "string",
-    "transition": "string",
-    "network_type": "string",
-    "type_network": "string",
-    "type_constellations": "int",
-    "level": "int",
-    "network": "string",
-    "dbm": "int",
-    "rssi": "int",
-    "level_wifi": "int",
-    "level_mobile": "int",
-}
-
-ALLOWABLE_SENSOR_VALUE_DTYPES = ["double", "int", "long", "string"]
-
-def has_invalid_schema(dynamic_record):
-    """Returns a `string` describing the failure reason if invalid, otherwise returns empty string."""
-    
-    # # When reading from s3 rather than from catalog, there are not explicit partitions, and this
-    # # check on partitions must be excluded.
-    # if not type(dynamic_record["partition_0"]) == str:
-    #     return False
-    # if not type(dynamic_record["partition_1"]) == str:
-    #     return False
-    # if not type(dynamic_record["partition_2"]) == str:
-    #     return False
-    # if not type(dynamic_record["partition_3"]) == str:
-    #     return False
-    # if not type(dynamic_record["partition_4"]) == str:
-    #     return False
-    
-    # Metadata that is always present must be checked.
-    if not type(dynamic_record["id_api_key"]) == str:
-        return "id_api_key bad dtype"
-    if not type(dynamic_record["body"]["metadata"]["id_phone"]) == str:
-        return "id_phone bad dtype"
-    if not type(dynamic_record["body"]["metadata"]["version_sw"]) == str:
-        return "version_sw bad dtype"
-    
-    # Not all metadata fields were present in all versions of the SDK;
-    # when they are present, its datatype must be checked; when they are present, the file can pass.
-    if "type_device" in dynamic_record["body"]["metadata"]:
-        if not type(dynamic_record["body"]["metadata"]["type_device"]) == str:
-            return "type_device bad dtype"
-    if "version_android" in dynamic_record["body"]["metadata"]:
-        if not type(dynamic_record["body"]["metadata"]["version_android"]) == str:
-            return "version_android bad dtype"
-    if "sensors_available" in dynamic_record["body"]["metadata"]:
-        field = dynamic_record["body"]["metadata"]["sensors_available"]
-        if isinstance(field, list):
-            for element in field:
-                if not isinstance(element, str):
-                    return "sensors_available bad dtype"
-        else:
-            return "sensors_available bad dtype"
-    if "permissions_granted" in dynamic_record["body"]["metadata"]:
-        field = dynamic_record["body"]["metadata"]["permissions_granted"]
-        if isinstance(field, list):
-            for element in field:
-                if not isinstance(element, str):
-                    return "permissions_granted bad dtype"
-        else:
-            return "permissions_granted bad dtype"
-    
-    # Check the schema of the data.
-    data_packet_list = dynamic_record["body"]["data"]
-    for data_packet in data_packet_list:
-        
-        field = data_packet["names"]
-        if isinstance(field, list):
-            for element in field:
-                if not isinstance(element, str):
-                    return "names bad dtype"
-        else:
-            return "names bad dtype"
-
-        field = data_packet["t_utc"]
-        if isinstance(field, list):
-            for element in field:
-                if not isinstance(element, float):
-                    return "t_utc bad dtype"
-                if (int(math.log10(int(element))) + 1) != 10:  # confirm that timestamps are valid format
-                    return "t_utc bad dtype"
-        else:
-            return "t_utc bad dtype"
-
-        field = data_packet["values"]
-        if not isinstance(field, list):
-            return "values bad dtype"
-        else:
-            for element in field:
-                if isinstance(element, list):
-                    for subelement in element:
-                        # Data values can be null.
-                        if (subelement is not None) and (type(subelement) not in {int, float, str}):
-                            return "values bad dtype"
-                else:
-                    return "values bad dtype"
-
-    return ""
-
-def add_has_invalid_schema_column(dynamic_record):
-    try:
-        dynamic_record_is_valid_schema = self.has_invalid_schema(dynamic_record)
-    except:
-        dynamic_record_is_valid_schema = ""
-    dynamic_record["has_invalid_schema"] = dynamic_record_is_valid_schema
-    return dynamic_record
 
 class FilterMergeGlueJob(BaseGlueJob):
     app_name = 'filter-merge-glue-job'
@@ -204,7 +20,8 @@ class FilterMergeGlueJob(BaseGlueJob):
                 connection_type="s3",
                 connection_options={
                     "paths": [
-                        "s3://true-v2-input/data/2023/11/30/22/",
+                        # "s3://true-v2-input/data/2023/11/30/22/",
+                        "s3://truemetrics-spark-test-data/filter-merge-glue-job/true-v2-input-files-correct-schema-all-dtypes-values/"
                     ],
                     "recurse": True,
                 },
@@ -222,7 +39,7 @@ class FilterMergeGlueJob(BaseGlueJob):
     def process_data(self, data: Union[RDD, DataFrame, DynamicFrame]) -> Union[RDD, DataFrame, DynamicFrame]:
         data_with_has_invalid_schema_column = (
             data
-            .map(add_has_invalid_schema_column)
+            .map(filter_merge_glue_job_utils.add_has_invalid_schema_column)
         )
         print(
             f"""All bad source files and their badness reason: \n{
@@ -320,7 +137,7 @@ class FilterMergeGlueJob(BaseGlueJob):
             current_schema = dict(valid_data_sensor_reading_timestamp_level.dtypes)
             sensor_target_schema_conversion_mapping = {
                 field: {"current_dtype_is_struct": current_schema[field].startswith("struct"), "desired_dtype": target_dtype}
-                for field, target_dtype in target_sensor_schema.items()
+                for field, target_dtype in filter_merge_glue_job_utils.TARGET_SENSOR_SCHEMA.items()
                 if field in current_schema.keys()
             }
             print("sensor_target_schema_conversion_mapping:")
---
+++ src/filter_merge_glue_job_utils.py
@@ -0,0 +1,174 @@
+import math
+
+
+TARGET_SENSOR_SCHEMA = {
+    "acc_x_sen": "double",
+    "acc_y_sen": "double",
+    "acc_z_sen": "double",
+    "angvel_x_sen": "double",
+    "angvel_y_sen": "double",
+    "angvel_z_sen": "double",
+    "mag_x_sen": "double",
+    "mag_y_sen": "double",
+    "mag_z_sen": "double",
+    "log": "string",
+    "confidence": "int",
+    "pres_abs": "double",
+    "ang_lat": "double",
+    "accu_ang_bearing": "double",
+    "vel": "double",
+    "ang_bearing": "double",
+    "accu_ver": "double",
+    "accu_hor": "double",
+    "accu_vel": "double",
+    "alt": "double",
+    "ang_lon": "double",
+    "ang_lat_gnss": "double",
+    "accu_ang_bearing_gnss": "double",
+    "vel_gnss": "double",
+    "ang_bearing_gnss": "double",
+    "accu_ver_gnss": "double",
+    "accu_hor_gnss": "double",
+    "accu_vel_gnss": "double",
+    "alt_gnss": "double",
+    "ang_lon_gnss": "double",
+    "customermetadata": "string",
+    "status": "string",
+    "svid": "int",
+    "type_constellation": "int",
+    "time_offset_ns": "double",
+    "state": "int",
+    "t_received_sv_ns": "double",
+    "t_received_sv_unc_ns": "double",
+    "cn0_db": "double",
+    "baseband_cn0_db": "double",
+    "pseudorange": "double",
+    "unc_pseudorange": "double",
+    "state_d_range_acc": "int",
+    "d_range_acc": "double",
+    "d_range_unc_acc": "double",
+    "f_carrier": "double",
+    "cycles_carrier": "double",
+    "phase_carrier": "double",
+    "unc_phase_carrier": "double",
+    "indicator_multipath": "int",
+    "snr_in_db": "double",
+    "auto_gain_ctrl_level": "double",
+    "type_code": "string",
+    "bias_full_inter_signal_ns": "double",
+    "unc_bias_full_inter_signal_ns": "double",
+    "bias_sat_inter_signal_ns": "double",
+    "unc_bias_sat_inter_signal_ns": "double",
+    "bias_code_phase_ns": "double",
+    "unc_bias_code_phase_ns": "double",
+    "bias_carrier_phase_ns": "double",
+    "unc_bias_carrier_phase_ns": "double",
+    "multipath_indic": "int",
+    "snr_in_db_gnss": "double",
+    "auto_gain_ctrl_level_gnss": "double",
+    "type_code_gnss": "int",
+    "bias_full_inter_signal_ns_gnss": "double",
+    "unc_bias_full_inter_signal_ns_gnss": "double",
+    "bias_sat_inter_signal_ns_gnss": "double",
+    "unc_bias_sat_inter_signal_ns_gnss": "double",
+    "level_battery": "double",
+    "status_battery": "string",
+    "health_battery": "string",
+    "count_step": "double",
+    "status_connectivity": "string",
+    "activity": "string",
+    "transition": "string",
+    "network_type": "string",
+    "type_network": "string",
+    "type_constellations": "int",
+    "level": "int",
+    "network": "string",
+    "dbm": "int",
+    "rssi": "int",
+    "level_wifi": "int",
+    "level_mobile": "int",
+}
+
+ALLOWABLE_SENSOR_VALUE_DTYPES = ["double", "int", "long", "string"]
+
+def has_invalid_schema(dynamic_record) -> str:
+    """Returns a `string` describing the failure reason if invalid, otherwise returns empty string."""
+    
+    # Metadata that is always present must be checked.
+    if not type(dynamic_record["id_api_key"]) == str:
+        return "id_api_key bad dtype"
+    if not type(dynamic_record["body"]["metadata"]["id_phone"]) == str:
+        return "id_phone bad dtype"
+    if not type(dynamic_record["body"]["metadata"]["version_sw"]) == str:
+        return "version_sw bad dtype"
+    
+    # Not all metadata fields were present in all versions of the SDK;
+    # when they are present, its datatype must be checked; when they are present, the file can pass.
+    if "type_device" in dynamic_record["body"]["metadata"]:
+        if not type(dynamic_record["body"]["metadata"]["type_device"]) == str:
+            return "type_device bad dtype"
+    if "version_android" in dynamic_record["body"]["metadata"]:
+        if not type(dynamic_record["body"]["metadata"]["version_android"]) == str:
+            return "version_android bad dtype"
+    if "sensors_available" in dynamic_record["body"]["metadata"]:
+        field = dynamic_record["body"]["metadata"]["sensors_available"]
+        if isinstance(field, list):
+            for element in field:
+                if not isinstance(element, str):
+                    return "sensors_available bad dtype"
+        else:
+            return "sensors_available bad dtype"
+    if "permissions_granted" in dynamic_record["body"]["metadata"]:
+        field = dynamic_record["body"]["metadata"]["permissions_granted"]
+        if isinstance(field, list):
+            for element in field:
+                if not isinstance(element, str):
+                    return "permissions_granted bad dtype"
+        else:
+            return "permissions_granted bad dtype"
+    
+    # Check the schema of the data.
+    data_packet_list = dynamic_record["body"]["data"]
+    for data_packet in data_packet_list:
+        
+        field = data_packet["names"]
+        if isinstance(field, list):
+            for element in field:
+                if not isinstance(element, str):
+                    return "names bad dtype"
+        else:
+            return "names bad dtype"
+
+        field = data_packet["t_utc"]
+        if isinstance(field, list):
+            for element in field:
+                if not isinstance(element, float):
+                    return "t_utc bad dtype"
+                if (int(math.log10(int(element))) + 1) != 10:  # confirm that timestamps are valid format
+                    return "t_utc bad dtype"
+        else:
+            return "t_utc bad dtype"
+
+        field = data_packet["values"]
+        if not isinstance(field, list):
+            return "values bad dtype"
+        else:
+            for element in field:
+                if isinstance(element, list):
+                    for subelement in element:
+                        # Data values can be null.
+                        if (subelement is not None) and (type(subelement) not in {int, float, str}):
+                            return "values bad dtype"
+                else:
+                    return "values bad dtype"
+
+    return ""
+
+
+def add_has_invalid_schema_column(dynamic_record):
+    try:
+        dynamic_record_is_valid_schema = has_invalid_schema(dynamic_record)
+    except:
+        dynamic_record_is_valid_schema = ""
+    dynamic_record["has_invalid_schema"] = dynamic_record_is_valid_schema
+    return dynamic_record
