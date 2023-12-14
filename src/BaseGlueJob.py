06621a226a1cbaabe98e9dc55c3922f8da746fbd
Benjamin Feifke <benjamin@truemetrics.io>
2023-12-14 13:24:27 +0100

    Moved file.

--- src/BaseGlueJob.py
+++ src/BaseGlueJob.py
@@ -1,13 +1,8 @@
 import sys
-from functools import reduce
-from operator import getitem
-import datetime as dt
-import math
 from typing import Union
 from pyspark import RDD
 
 from pyspark.context import SparkContext
-from pyspark.sql.functions import lit, explode, arrays_zip, col, array_contains, udf, coalesce, expr, posexplode, first, count, from_unixtime, year, month, dayofmonth, hour, input_file_name
 from pyspark.sql import DataFrame
 
 from awsglue.transforms import *
@@ -18,26 +13,26 @@ from awsglue.dynamicframe import DynamicFrame
 
 
 class BaseGlueJob:
-  app_name: str = None
+    app_name: str = None
 
-  def __init__(self) -> None:
-    self.job_args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-    self.sc = SparkContext(appName=self.app_name)
-    self.glueContext = GlueContext(self.sc)
-    self.glue_job = Job(self.glueContext)
-  
-  def load_data(self) -> Union[RDD, DataFrame, DynamicFrame]:
-    raise NotImplementedError
+    def __init__(self) -> None:
+        self.job_args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+        self.sc = SparkContext(appName=self.app_name)
+        self.glueContext = GlueContext(self.sc)
+        self.glue_job = Job(self.glueContext)
+    
+    def load_data(self) -> Union[RDD, DataFrame, DynamicFrame]:
+        raise NotImplementedError
 
-  def process_data(self, data: Union[RDD, DataFrame, DynamicFrame]) -> Union[RDD, DataFrame, DynamicFrame]:
-    raise NotImplementedError
-  
-  def write_data(self, data: Union[RDD, DataFrame, DynamicFrame]) -> None:
-    raise NotImplementedError
-  
-  def execute_job(self) -> None:
-    self.glue_job.init(self.job_args['JOB_NAME'], self.job_args)
-    data = self.load_data()
-    data = self.process_data(data)
-    self.write_data(data)
-    self.glue_job.commit()
+    def process_data(self, data: Union[RDD, DataFrame, DynamicFrame]) -> Union[RDD, DataFrame, DynamicFrame]:
+        raise NotImplementedError
+    
+    def write_data(self, data: Union[RDD, DataFrame, DynamicFrame]) -> None:
+        raise NotImplementedError
+    
+    def execute_job(self) -> None:
+        self.glue_job.init(self.job_args['JOB_NAME'], self.job_args)
+        data = self.load_data()
+        data = self.process_data(data)
+        self.write_data(data)
+        self.glue_job.commit()
---
+++ src/filter-merge-glue-job.py
@@ -0,0 +1,33 @@
+from typing import Union
+from pyspark import RDD
+
+from pyspark.sql import DataFrame
+from BaseGlueJob import BaseGlueJob
+
+from awsglue.dynamicframe import DynamicFrame
+
+
+class FilterMergeGlueJob(BaseGlueJob):
+    app_name = 'filter-merge-glue-job'
+  
+    def load_data(self) -> Union[RDD, DataFrame, DynamicFrame]:
+        data = (
+            self.glueContext
+            .create_sample_dynamic_frame_from_catalog(
+                database='true-schema-input-v2',
+                table_name='true_v2_input',
+                num=10,
+            )
+        )
+        return data
+
+    def process_data(self, data: Union[RDD, DataFrame, DynamicFrame]) -> Union[RDD, DataFrame, DynamicFrame]:
+        return data
+    
+    def write_data(self, data: Union[RDD, DataFrame, DynamicFrame]) -> None:
+        data.printSchema()
+
+
+if __name__ == "__main__":
+    job = FilterMergeGlueJob()
+    job.execute_job()
