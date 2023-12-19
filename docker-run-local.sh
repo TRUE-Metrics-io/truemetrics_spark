a787b6a662960b5e7a2cb720694b4ef624476100
Benjamin Feifke <benjamin@truemetrics.io>
2023-12-19 20:24:57 +0100

    Added extra line to docker-run-local.sh.

--- docker-run-local.sh
+++ docker-run-local.sh
@@ -9,4 +9,5 @@ docker run -it \
     -p 4040:4040 \
     -p 18080:18080 \
     --name glue_spark_submit amazon/aws-glue-libs:glue_libs_4.0.0_image_01 \
-    spark-submit /home/glue_user/workspace/$JOB_NAME.py --JOB_NAME $JOB_NAME
+    spark-submit /home/glue_user/workspace/$JOB_NAME.py \
+    --JOB_NAME $JOB_NAME
